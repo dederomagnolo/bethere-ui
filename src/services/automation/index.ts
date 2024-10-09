ef69fa3686411f38bdd2875bc1d1fb306a87905a
Debora Romagnolo <d.romagnolo@hotmail.com>
2024-10-09 11:11:23 -0300

    WIP

--- src/services/automation/index.ts
+++ src/services/automation/index.ts
@@ -1,6 +1,7 @@
 
+import { Automation } from 'types/interfaces'
 import callApi from '../callApi'
-import { FetchAutomationRoutinesProps } from './types'
+import { CreateAutomationProgram, FetchAutomationRoutinesProps } from './types'
 
 export const fetchAutomationRoutines = async ({
   token
@@ -22,96 +23,29 @@ export const fetchAutomationRoutines = async ({
   }
 }
 
-// export const createAlert = async ({
-//   token,
-//   deviceId,
-//   sensorId,
-//   alertName,
-//   paramType,
-//   value,
-//   operator,
-// }: CreateAlertProps) => {
-//   try {
-//     const res = await callApi({
-//       payload: {
-//         deviceId,
-//         sensorId,
-//         alertName,
-//         paramType,
-//         value,
-//         operator
-//       },
-//       method: 'POST',
-//       service: '/alerts/create',
-//       token,
-//       showToast: true
-//     })
+export const createAutomationProgram = async ({
+  token,
+  name,
+  type,
+  timer,
+  trigger
+}: CreateAutomationProgram) => {
+  try {
+    const res = await callApi({
+      payload: {
+        name,
+        type,
+        timer,
+        trigger
+      },
+      method: 'POST',
+      service: '/automation/create',
+      token,
+      showToast: true
+    })
 
-//     return res
-//   } catch (err: unknown) {
-//     throw new Error('400');
-//   }
-// }
-
-// export const editAlert = async ({
-//   token,
-//   deviceId,
-//   sensorId,
-//   alertName,
-//   paramType,
-//   value,
-//   alertId,
-//   operator
-// }: EditAlertProps) => {
-//   try {
-//     const res = await callApi({
-//       payload: {
-//         deviceId,
-//         sensorId,
-//         alertName,
-//         paramType,
-//         value,
-//         operator,
-//         alertId
-//       },
-//       method: 'POST',
-//       service: '/alerts/edit',
-//       token,
-//       showToast: true
-//     })
-
-//     return res
-//   } catch (err: unknown) {
-//     // if (err instanceof Error) {
-//     //   return {
-//     //     message: `Error: (${err.message})`,
-//     //   };
-//     // }
-//     throw new Error('400');
-//   }
-// }
-
-// export const deleteAlert = async ({
-//   token,
-//   deviceId,
-//   sensorId,
-//   alertId
-// }: DeleteAlertProps) => {
-//   try {
-//     const res = await callApi({
-//       payload: {
-//         deviceId,
-//         sensorId,
-//         alertId
-//       },
-//       method: 'POST',
-//       service: '/alerts/delete',
-//       token,
-//       showToast: true
-//     })
-
-//     return res
-//   } catch (err: unknown) {
-//     throw new Error('400');
-//   }
-// }
+    return res
+  } catch (err: unknown) {
+    throw new Error('400');
+  }
+}
